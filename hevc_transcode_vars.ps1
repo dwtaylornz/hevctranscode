642397e37ba96e6088e251fca9b1c8938304bdf7
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-12-09 08:05:51 +1300

    Update hevc_transcode_vars.ps1

--- hevc_transcode_vars.ps1
+++ hevc_transcode_vars.ps1
@@ -3,7 +3,7 @@ $scan_period = "1440" # max minutes before doing a re-scan
 $move_file = 1 # set to 0 for testing
 $hevc_offload = "AMD" # set to AMD, NVIDIA or CPU
 $ffmpeg_logging = "error" # info, error
-$convert_1080p = 1 # 1 will convert higher resolution videos down to 1080p , 0 will not
+$convert_1080p = 0 # 1 will convert higher resolution videos down to 1080p , 0 will not
 $video_directory = "z:\videos\" # path in SMB share 
 $ffmpeg_path = "C:\temp\ffmpeg\bin" # where ffmpeg lives
 
