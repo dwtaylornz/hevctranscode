434b9e4744cfbbaefea0b139b52dfdff84c5752f
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-01-02 17:38:21 +1300

    Update hevc_transcode.ps1

--- hevc_transcode.ps1
+++ hevc_transcode.ps1
@@ -5,7 +5,7 @@
 # populate hevc_transcode_vars.ps1 before running this script. 
 
 # grab variables from var file 
-. .\hevc_transcode_vars.ps1
+. .\hevc_transcode_variables.ps1
 
 Set-Location $ffmpeg_path
 
@@ -24,20 +24,25 @@ while ($true) {
     
     Write-Host ""
 
-    # Get largest files
-    if ($scan_at_start -eq 1){
-        Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
-        $videos = Get-ChildItem -r $video_directory -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m | Sort-Object -descending -Property length | Select-Object Fullname, name, length
-        $file_count = $videos.Count
-        $videos | Export-Csv ./scan_results.csv
-        Write-Host "Done ($file_count)" 
-        }
+   # Scan or retrive $videos 
+    #. .\hevc_transcode_scan.ps1
+    
+    # Remove-Job -Name "Scan" -Force | Out-Null
+
+    if (-not(test-path -PathType leaf .\scan_results.csv) -or $scan_at_start -eq 1) { 
+        # Write-Host  Write-Host "Forcing scan" 
+        Start-Job -Name "Scan" -FilePath .\scripts\hevc_transcode_scan.ps1 -ArgumentList $ffmpeg_path | Out-Null
+        Receive-Job -name "Scan" -wait
+    }
+
     else {
-        Write-Host -NoNewline "Getting previous scan results)..." 
+        Write-Host -NoNewline "Getting previous scan results & running scan in background..." 
         $videos = Import-Csv -Path .\scan_results.csv
         $file_count = $videos.Count
-        Write-Host "Done ($file_count)" 
+        Write-Host "Done ($file_count)"
+        Start-Job -Name "Scan" -FilePath .\scripts\hevc_transcode_scan.ps1 -ArgumentList $ffmpeg_path | Out-Null
     }
+    
 
     # Get previously skipped files
     Write-Host -NoNewline "Getting previously skipped or completed files..." 
@@ -78,98 +83,12 @@ while ($true) {
             }  
         }
 
-        if ($skip -eq 0) {            
-
-            #check if file is HEVC first 
-            $video_codec = $null 
-            $video_codec = (./ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "`"$video_path"`") | Out-String
-            if (Select-String -pattern "hevc" -InputObject $video_codec -quiet) { $video_codec = "hevc" }
-            if (Select-String -pattern "h264" -InputObject $video_codec -quiet) { $video_codec = "h264" } 
-            if (Select-String -pattern "vc1" -InputObject $video_codec -quiet) { $video_codec = "vc1" }          
-            if (Select-String -pattern "mpeg2video" -InputObject $video_codec -quiet) { $video_codec = "mpeg2video" }
-            if (Select-String -pattern "mpeg4" -InputObject $video_codec -quiet) { $video_codec = "mpeg4" }
-
-            #check video width (1920 width is more consistant for 1080p videos)
-            $video_width = $null 
-            $video_width = (./ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
-            $video_width = $video_width.trim()
-            $video_width = $video_width -as [Int]
-            #$video_width = $video_width.substring(0, 4)
-
-            #check video length (used for progress updates)
-            $video_duration = $null 
-            $video_duration = (./ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
-            $video_duration = $video_duration.trim()
-            $video_duration_formated = [timespan]::fromseconds($video_duration)
-            $video_duration_formated = ("{0:hh\:mm\:ss}" -f $video_duration_formated)
-            #$video_duration = $video_duration -as [Int]        
-
-            Write-Host ""
-            Write-Host "Processing : $video_name"
-            Write-Host "  Size (GB) : $video_size, Codec : $video_codec, Width : $video_width, Duration : $video_duration_formated"             
-
-            $start_time = (GET-Date)
-
-            $convert_error = 1 
-
-            #AMD Offload...
-            if ($convert_1080p -eq 1 -AND $video_width -gt 1920 -AND $hevc_offload -eq "AMD") { 
-                Write-Host -NoNewline "  Attempting transcode via AMD to 1080p HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -vf scale=1920:-1 -map 0 -c:v hevc_amf -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE     
-
-            }
-
-            elseif ($video_codec -ne "hevc" -AND $hevc_offload -eq "AMD") { 
-                Write-Host -NoNewline "  Attempting transcode via AMD to HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -map 0 -c:v hevc_amf -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE
-                
-            }
-
-            #Nvidia Offload... 
-            elseif ($convert_1080p -eq 1 -AND $video_width -gt 1920 -AND $hevc_offload -eq "Nvidia") { 
-                Write-Host -NoNewline "  Attempting transcode via Nvidia to 1080p HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -vf scale=1920:-1 -map 0 -c:v hevc_nvenc -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE     
-
-            }
-
-            elseif ($video_codec -ne "hevc" -AND $hevc_offload -eq "Nvidia") { 
-                Write-Host -NoNewline "  Attempting transcode via Nvidia to HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -map 0 -c:v hevc_nvenc -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE
-                
-            }
-
-            #CPU... (evaluated last) 
-            elseif ($convert_1080p -eq 1 -AND $video_width -gt 1920) { 
-                Write-Host -NoNewline "  Attempting transcode via CPU to 1080p HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -vf scale=1920:-1 -map 0 -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE     
-
-            }
-
-            elseif ($video_codec -ne "hevc") { 
-                Write-Host -NoNewline "  Attempting transcode via CPU to HEVC (this may take some time, started $start_time)..."            
-                ./ffmpeg -hide_banner -v $ffmpeg_logging -y -i $video_path -map 0 -c:a copy -c:s copy -gops_per_idr 1 -max_muxing_queue_size 9999 output\$video_name
-                $convert_error = $LASTEXITCODE
-                
-            }
-
-            
-            $end_time = (GET-Date)
-
-            #calc time taken 
-            $time = $end_time - $start_time
-            $time_hours = $time.hours
-            $time_mins = $time.minutes
-            $time_secs = $time.seconds
-            $total_time_formated = "$time_hours" + ":" + "$time_mins" + ":" + "$time_secs" 
-            $run_time = $end_time - $run_start
-            $run_time_current = $run_time.minutes + ($run_time.hours * 60)
-
-            Write-Output "$video_name ($count/$file_count, $run_time_current/$scan_period)" >> transcode.log            
+        if ($skip -eq 0) {   
+        
+        
+            # run background transcode
+            . .\scripts\hevc_transcode_background.ps1         
+       
 
             if ($convert_error -eq 0) {          
 
@@ -189,8 +108,8 @@ while ($true) {
                 Write-Host "  Duration : $total_time_formated, GB Saved : $diff ($video_size -> $video_new_size) or $diff_percent percent"
                        
 
-                #confirm move file is enabled, and confirm file is 30% smaller or non-zero 
-                if ($move_file -eq 1 -AND $diff_percent -gt 30 -AND $diff_percent -lt 95 -AND $video_new_size -ne 0 -AND $diff -gt 0) {                  
+                #confirm move file is enabled, and confirm file is 5% smaller or non-zero 
+                if ($move_file -eq 1 -AND $diff_percent -gt 5 -AND $diff_percent -lt 95 -AND $video_new_size -ne 0 -AND $diff -gt 0) {                  
 
                     $delay = 10 
                     Write-Host -NoNewline "  Sleep before copy ($delay seconds)..."
@@ -250,5 +169,5 @@ while ($true) {
         $sleep_time_secs = $sleep_time * 60
         Start-Sleep $sleep_time_secs
     }
-    $scan_at_start = 1 
+ 
 }
