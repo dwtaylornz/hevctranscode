62b02405e1ed639d6e60f0679343c443d6b2fff2
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-01-12 14:21:44 +1300

    Update and rename hevc_transcode_scan.ps1 to job_media_scan.ps1

---
+++ job_media_scan.ps1
@@ -0,0 +1,14 @@
+# Get largest files
+
+Set-Location $args[0]
+
+# grab variables from var file 
+. .\hevc_transcode_variables.ps1
+
+Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
+$videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m | Sort-Object -descending -Property length | Select-Object Fullname, name, length
+$file_count = $videos.Count
+$videos | Export-Csv ./scan_results.csv
+#Write-Host "Done ($file_count)" 
+
+Remove-Job -Name Scan -ea silentlycontinue
