656359b9b2735cf298a160e40abefb8946946a27
Darren <dwtaylornz@gmail.com>
2021-04-14 15:18:54 +1200

    remove stuff

--- job_transcode_progress.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-$state = Get-Content ..\test.txt -Tail 1 | Select-String -Pattern 'time'
-
-$Delimiter=" ";
-$Array=$state -Split $Delimiter;
-$time_taken = Write-Output $Array[8];
-$time_taken = $time_taken.trim()
