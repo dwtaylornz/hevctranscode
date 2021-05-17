e0376d538afc7cfeb7333f6d692adca3a87a0256
Darren <dwtaylornz@gmail.com>
2021-05-17 13:18:02 +1200

    update

--- job_media_scan.ps1
+++ job_media_scan.ps1
@@ -6,8 +6,8 @@ Set-Location $args[0]
 . .\hevc_transcode_variables.ps1
 
 #Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
-$videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m, *.m2ts| Sort-Object -descending -Property length | Select-Object Fullname, name, length
-$file_count = $videos.Count
+$videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m, *.m2ts | Sort-Object -descending -Property length | Select-Object Fullname, name, length
+#$file_count = $videos.Count
 $videos | Export-Csv ./scan_results.csv
 
 #Write-Host "Done ($file_count)" 
