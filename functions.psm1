d0f1900942f4bd194be15fd0e8a158f553e056ce
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-06-21 14:31:44 +1200

    Update functions.psm1

--- functions.psm1
+++ functions.psm1
@@ -1,5 +1,6 @@
 function Trace-Message([string] $message) {
     Write-Output "$(Get-Date -Format G): $message"
+    Write-Output "$(Get-Date -Format G): $message" >> hevctranscode.log
 }
 
 Export-ModuleMember -Function *
