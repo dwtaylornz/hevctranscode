ece0d11fc11cb5a131eaac2112a88f715d87d69b
Darren <dwtaylornz@gmail.com>
2021-07-17 08:26:58 +1200

    update

--- functions.psm1
+++ functions.psm1
@@ -1,6 +1,37 @@
-function Trace-Message([string] $message) {
+function Trace-Message ([string] $message) {
     Write-Output "$(Get-Date -Format G): $message"
     Write-Output "$(Get-Date -Format G): $message" >> hevc_transcode.log
+}
+
+function Get-VideoCodec ([string] $video_path) {
+    #Write-Host "Check if file is HEVC first..."
+    $video_codec = (.\ffprobe.exe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "`"$video_path"`") | Out-String
+    if (Select-String -pattern "hevc" -InputObject $video_codec -quiet) { $video_codec = "hevc" }
+    if (Select-String -pattern "h264" -InputObject $video_codec -quiet) { $video_codec = "h264" } 
+    if (Select-String -pattern "vc1" -InputObject $video_codec -quiet) { $video_codec = "vc1" }          
+    if (Select-String -pattern "mpeg2video" -InputObject $video_codec -quiet) { $video_codec = "mpeg2video" }
+    if (Select-String -pattern "mpeg4" -InputObject $video_codec -quiet) { $video_codec = "mpeg4" }
+    if (Select-String -pattern "rawvideo" -InputObject $video_codec -quiet) { $video_codec = "rawvideo" }
+    return $video_codec
+}
+function Get-VideoWidth ([string] $video_path) {
+    #check video width (1920 width is more consistant for 1080p videos)
+    $video_width = $null 
+    $video_width = (.\ffprobe.exe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
+    $video_width = $video_width.trim()
+    $video_width = $video_width -as [Int]
+    return $video_width
+}
+
+
+function Get-VideoDuration ([string] $video_path) {
+    #check video length
+    $video_duration = $null 
+    $video_duration = (.\ffprobe.exe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
+    $video_duration = $video_duration.trim()
+    $video_duration_formated = [timespan]::fromseconds($video_duration)
+    $video_duration_formated = ("{0:hh\:mm\:ss}" -f $video_duration_formated)    
+    return $video_duration
 }
 
 Export-ModuleMember -Function *
--- job_transcode.ps1
+++ job_transcode.ps1
@@ -34,32 +34,18 @@ Foreach ($video in $videos) {
     if ($skip -eq 0) {   
 
         #Write-Host "Check if file is HEVC first..."
-        $video_codec = $null 
-        $video_codec = (.\ffprobe.exe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "`"$video_path"`") | Out-String
-        if (Select-String -pattern "hevc" -InputObject $video_codec -quiet) { $video_codec = "hevc" }
-        if (Select-String -pattern "h264" -InputObject $video_codec -quiet) { $video_codec = "h264" } 
-        if (Select-String -pattern "vc1" -InputObject $video_codec -quiet) { $video_codec = "vc1" }          
-        if (Select-String -pattern "mpeg2video" -InputObject $video_codec -quiet) { $video_codec = "mpeg2video" }
-        if (Select-String -pattern "mpeg4" -InputObject $video_codec -quiet) { $video_codec = "mpeg4" }
-        if (Select-String -pattern "rawvideo" -InputObject $video_codec -quiet) { $video_codec = "rawvideo" }
+        $video_codec = Get-VideoCodec $video_path
 
         #check video width (1920 width is more consistant for 1080p videos)
-        $video_width = $null 
-        $video_width = (.\ffprobe.exe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
-        $video_width = $video_width.trim()
-        $video_width = $video_width -as [Int]
+        $video_width = Get-VideoWidth $video_path
 
-        #check video length
-        $video_duration = $null 
-        $video_duration = (.\ffprobe.exe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1  "`"$video_path"`") | Out-String
-        $video_duration = $video_duration.trim()
-        $video_duration_formated = [timespan]::fromseconds($video_duration)
-        $video_duration_formated = ("{0:hh\:mm\:ss}" -f $video_duration_formated)    
+        #check video duration 
+        $video_duration = Get-VideoDuration $video_path
 
         if ($video_size -lt $min_video_size){
             Trace-Message "Video smaller than minium of $min_video_size, exiting..."
             Start-Sleep 10   
-            exit}
+            throw "bye"}
           
         $start_time = (GET-Date)
    
