2549aa333acaaf2dc3ad5ff6b35d0dffd96d3b82
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-07-26 14:33:40 +1200

    Dev (#5)

--- hevc_transcode.ps1
+++ hevc_transcode.ps1
@@ -2,7 +2,7 @@
 # github.com/dwtaylornz/hevctranscode
 #
 # script will continously loop through videos transcoding to HEVC
-# populate hevc_transcode_vars.ps1 before running this script. 
+# populate hevc_transcode_variables.ps1 before running this script. 
 
 $RootDir = Get-Location
 
@@ -10,149 +10,115 @@ Clear-Host
 
 Import-Module ".\functions.psm1" -Force
 
-. .\hevc_transcode_variables.ps1
 # Get-Variables
+. .\hevc_transcode_variables.ps1
 
 #map media drive 
-while (!(test-path -PathType container $media_path) -AND $smb_enabled -eq "true") {
-    Start-Sleep 2
-    Write-Host -NoNewline "Mapping Drive (smb enabled)... "     
-    net use $smb_driveletter \\$smb_server\$smb_share /user:$smb_user $smb_password | Out-Null   
+if ($smb_enabled) {
+    while (!(test-path -PathType container $media_path) ) {
+        Start-Sleep 2
+        Write-Host -NoNewline "Mapping Drive (smb enabled)... "     
+        net use $smb_driveletter \\$smb_server\$smb_share /user:$smb_user $smb_password | Out-Null   
+    }  
+
 }
 
 # Setup temp output folder, and clear previous transcodes
 if (!(test-path -PathType container output)) { new-item -itemtype directory -force -path output | Out-Null }
-
-
-# Write-Host "Checking for any existing running jobs..." 
-if ( [bool](get-job -Name GPU-Transcode -ea silentlycontinue) ) {
-    $gpu_state = (get-job -Name GPU-Transcode).State 
-    if ($gpu_state -eq "Running") {    
-        Write-Host "GPU Job - Please wait, job already exists and" $gpu_state -ForegroundColor Yellow
-    }
-}
-
-if ( [bool](get-job -Name CPU-Transcode -ea silentlycontinue) ) {
-    $cpu_state = (get-job -Name CPU-Transcode).State 
-    if ($cpu_state -eq "Running") {  
-        Write-Host "CPU Job - Please wait, job already exists and" $cpu_state -ForegroundColor Yellow
-    }
-}
-
-if ( [bool](get-job -Name Scan -ea silentlycontinue) ) {
-    $scan_state = (get-job -Name Scan).State 
-    if ($scan_state -eq "Running") {  
-        Write-Host "Scan Job - Please wait, job already exists and" $scan_state -ForegroundColor Yellow
-    }
-}
-
-# Main Loop 
-while ($true) {
     
-    Write-Host " "
+Write-Host " "
    
-    $start_time = (GET-Date)
+# run Scan job at $media_path or retrive videos from .\scan_results
 
-    # run Scan job at $media_path or retrive videos from .\scan_results
+if (-not(test-path -PathType leaf .\scan_results.csv) -or $scan_at_start -eq 1) { 
+    Write-Host  -NoNewline "Running file scan..." 
+    Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null
+    Receive-Job -name "Scan" -wait
+}
 
-    if (-not(test-path -PathType leaf .\scan_results.csv) -or $scan_at_start -eq 1) { 
-        Write-Host  -NoNewline "Running file scan..." 
-        Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null
-        Receive-Job -name "Scan" -wait
-    }
-
-    else {
-        Write-Host -NoNewline "Getting previous scan results & running new scan in background..." 
+else {
+    Write-Host -NoNewline "Getting previous scan results & running new scan in background..." 
         
-        if ( [bool](get-job -Name Scan -ea silentlycontinue) ) {
-            $scan_state = (get-job -Name Scan).State 
-            if ($scan_state -ne "Running") { 
-                remove-job -name Scan 
-                Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null
-            }   
-        }
-        else {
-
+    if ( [bool](get-job -Name Scan -ea silentlycontinue) ) {
+        $scan_state = (get-job -Name Scan).State 
+        if ($scan_state -ne "Running") { 
+            remove-job -name Scan 
             Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null
-        }
+        }   
+    }
+    # else { Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null }
         
+}
+    
+$videos = Import-Csv -Path .\scan_results.csv   
+$file_count = $videos.Count
+Write-Host "Done ($file_count)" 
+
+# Get previously skipped files from skip.log 
+
+Write-Host -NoNewline "Getting previously skipped or completed files..." 
+if ((test-path -PathType leaf skip.log)) { 
+    $skipped_files = Get-Content -Path skip.log 
+    $skip_count = $skipped_files.Count
+}
+else { $skip_count = 0 }
+    
+# Show total videos to process (scanned files - skip count) 
+$video_count = ($file_count - $skip_count)
+Write-Host "Done ($skip_count)"
+Write-Host ""
+Trace-Message "Total videos to process : $video_count"
+
+if ((test-path -PathType leaf skip.log)) { $skipped_files = Get-Content -Path skip.log }
+else {$skipped_files = ""}
+
+get-job -State Running
+Write-Host " "
+
+Foreach ($video in $videos) {
+
+    
+    $video_size = [math]::Round($video.length / 1GB, 2)
+    
+    if($video_size -lt $min_video_size){
+        Trace-Message "ALL DONE - Video smaller than defined $min_video_size, quiting"
+        pause
+        Break
     }
-    
-    $videos = Import-Csv -Path .\scan_results.csv
-    $cpu_videos = $videos.Clone()
-    [array]::Reverse($cpu_videos)
-    
-    $file_count = $videos.Count
-    Write-Host "Done ($file_count)" 
 
-    # Get previously skipped files from skip.log 
+    $video_name = $video.name
 
-    Write-Host -NoNewline "Getting previously skipped or completed files..." 
-    if ((test-path -PathType leaf skip.log)) { 
-        $skipped_files = Get-Content -Path skip.log 
-        $skip_count = $skipped_files.Count
-    }
-    else { $skip_count = 0 }
-    
-    # Show total videos to process (scanned files - skip count) 
-    $video_count = ($file_count - $skip_count)
-    Write-Host "Done ($skip_count)"
-    Write-Host ""
-    Trace-Message "Total videos to process : $video_count. Time before next scan : $scan_period minutes"
-    Write-Host ""
+    if ($video_name -notin $skipped_files) {
 
-    while ($true) {
+        while ($true) {
 
-        # Job Checker 
-        # Write-Host  "- second loop" 
-        # Start-Sleep 1
+            $done = 0
 
-        # GPU Transcode 
-        if ( [bool](get-job -Name GPU-Transcode -ea silentlycontinue) ) {
-            $gpu_state = (get-job -Name GPU-Transcode).State 
-            #Write-Host "  GPU Job exists and" $gpu_state
-            Receive-Job -name "GPU-Transcode"
-            if ($gpu_state -eq "Completed" -OR $gpu_state -eq "Stopped") { remove-job -name GPU-Transcode -Force }   
-        }
-        else {
-            #Write-Host "  GPU Job doesnt exist" 
-            Start-Job -Name "GPU-Transcode" -FilePath .\job_transcode.ps1 -ArgumentList $RootDir, $videos, "GPU" | Out-Null
-             
+            for ($thread = 1; $thread -le $GPU_threads; $thread++) {
+                # get thread state 
+                $gpu_state = Get-JobStatus "GPU-Transcode-$thread"
+
+                # clear completed or stopped jobs 
+                if ($gpu_state -eq "Completed" -OR $gpu_state -eq "Stopped") { 
+                    Receive-Job -name "GPU-Transcode-$thread" 
+                    remove-job -name "GPU-Transcode-$thread" -Force 
+                }   
+
+                # Output existing jobs 
+                if ($gpu_state -eq "Running" ) { 
+                    Receive-Job -name "GPU-Transcode-$thread" 
+                }   
+
+                # If thread not running then i can run it here 
+                if ($gpu_state -ne "Running") {
+                    Start-Job -Name "GPU-Transcode-$thread" -FilePath .\job_transcode.ps1 -ArgumentList $RootDir, $video, "GPU($thread)" | Out-Null 
+                    $done = 1 
+                    break
+                }            
+            }          
+
+            if ($done -eq 1) { break }
         }
 
-        if ($parallel_cpu_transcode -eq 1) {
-            #CPU Transcode 
-            if ( [bool](get-job -Name CPU-Transcode -ea silentlycontinue) ) {
-                $cpu_state = (get-job -Name CPU-Transcode).State 
-                # Write-Host "  CPU Job exists and" $cpu_state
-                Receive-Job -name "CPU-Transcode" 
-                if ($cpu_state -eq "Completed" -OR $cpu_state -eq "Stopped") { remove-job -name CPU-Transcode -Force }    
-            }
-            else {
-                # Write-Host "  CPU Job doesnt exist" 
-                Start-Job -Name "CPU-Transcode" -FilePath .\job_transcode.ps1 -ArgumentList $RootDir, $cpu_videos, "CPU" | Out-Null
-            }
-        }
-
-        $current_time = (GET-Date)
-        $timer = New-TimeSpan -Start $start_time -End $current_time
-        $time_total_min = $timer.minutes + ($timer.hours * 60)
-
-        #Write-Host "Start Time : $start_time Current Time : $current_time Time so far in min : $time_total_min"
-
-        if ($time_total_min -ge $scan_period) {
-            Write-Host ""
-            Write-Host -Nonewline "Scan period Expired - "
-            Remove-Job Scan -ea silentlycontinue
-            Start-Job -Name "Scan" -FilePath .\job_media_scan.ps1 -ArgumentList $RootDir | Out-Null
-            Receive-Job -name "Scan" -wait
-            Write-Host "Done" 
-            Write-Host ""
-            $videos = Import-Csv -Path .\scan_results.csv
-            $cpu_videos = $videos.Clone()
-            [array]::Reverse($cpu_videos)
-            $start_time = (GET-Date)
-
-        }
     }
 }
--- hevc_transcode_variables_EXAMPLE.ps1
+++ hevc_transcode_variables_EXAMPLE.ps1
@@ -1,23 +1,21 @@
 # Variables EXAMPLE, please rename to hevc_transcode_variables.ps1
 
 # Paths 
-$ffmpeg_path = "C:\temp\ffmpeg\bin" # where ffmpeg lives
 $media_path = "Z:\videos\" # path in SMB share (must include trailing backslash) 
 
 # Scanning 
 $scan_at_start = 0 # wait for scan files to complete at start of script
-$scan_period = "1440" # max minutes before doing a re-scan
-# $scan_disable = 0 # Use this if you have multiple machines running in same directory
 
 # transcode and processing 
 $ffmpeg_codec = "hevc_nvenc" # set to hevc_amf for AMD, hevc_nvenc for Nvidia, libx265 for CPU
-$move_file = 1 # set to 0 for testing (check .\output directory in ffmpeg_path) 
+$move_file = 1 # set to 0 for testing (check .\output directory) 
 $ffmpeg_logging = "error" # info, error
 $convert_1080p = 1 # 1 will convert higher resolution videos down to 1080p , 0 will not
-$parallel_cpu_transcode = 0 # set to 1 to enable parallel cpu transcoding (EXPERIMENTAL) 
+$min_video_size = 0 # min size in GB of video before it will quit
+$GPU_threads = 2 # how many GPU jobs at same time 
 
-# SMB config
-$smb_enabled = "true" # Set to true to map SMB drive
+# SMB config - if you want to map drive 
+$smb_enabled = "false" # Set to true to map SMB drive
 $smb_driveletter = "z:" # drive letter to map. Include colon. 
 $smb_server = "server" # SMB server 
 $smb_share = "share" # SMB share
