a14305fee6ff6d8c6e1a1a935984076a7f932482
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-07-31 09:50:39 +1200

    Dev (#8)

--- include/job_media_scan.ps1
+++ include/job_media_scan.ps1
@@ -1,5 +1,3 @@
-<<<<<<< HEAD:include/job_media_scan.ps1
-﻿# set root directory
 Set-Location $args[0]
 
 # grab variables from var file 
@@ -8,14 +6,3 @@ Set-Location $args[0]
 #Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
 $videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m, *.m2ts | Sort-Object -descending -Property length | Select-Object Fullname, name, length
 $videos | Export-Csv ./scan_results.csv -Encoding utf8
-=======
-﻿# set root directory
-Set-Location $args[0]
-
-# grab variables from var file 
-. .\hevc_transcode_variables.ps1
-
-#Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
-$videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m, *.m2ts | Sort-Object -descending -Property length | Select-Object Fullname, name, length
-$videos | Export-Csv ./scan_results.csv
->>>>>>> 646586e567f557bc6075a2e7f57e812b0f3b0501:job_media_scan.ps1
