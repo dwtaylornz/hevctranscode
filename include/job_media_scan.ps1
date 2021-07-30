1726846274741343dd76c2b0e398fde9f1586aab
Darren <dwtaylornz@gmail.com>
2021-07-31 10:07:40 +1200

    .

--- include/job_media_scan.ps1
+++ include/job_media_scan.ps1
@@ -1,8 +1,8 @@
 Set-Location $args[0]
 
 # grab variables from var file 
-. ..\hevc_transcode_variables.ps1
+. .\hevc_transcode_variables.ps1
 
 #Write-Host -NoNewline "Checking all video files and sizes (sorting largest to smallest)..." 
 $videos = Get-ChildItem -r $media_path -Include *.mkv, *.avi, *.mp4, *.ts, *.mov, *.y4m, *.m2ts | Sort-Object -descending -Property length | Select-Object Fullname, name, length
-$videos | Export-Csv ./scan_results.csv -Encoding utf8
+$videos | Export-Csv .\scan_results.csv -Encoding utf8
