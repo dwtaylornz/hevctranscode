0f22c1d21a7667b7f497e76e6349cc3266cd353c
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2021-08-02 12:55:49 +1200

    Dev (#16)
    * update
    
    * update
    
    * updates
    
    * update
    
    * Create functions.psm1
    
    * Update functions.psm1
    
    * Update hevc_transcode.ps1
    
    * Update functions.psm1
    
    * a bit of functions
    
    * fixed scanning issue
    
    * doh
    
    * fixed
    
    * background checks
    
    * check
    
    * aha
    
    * aha
    
    * another cahnge
    
    * update
    
    * update
    
    * update
    
    * aha
    
    * new!
    
    * update
    
    * add min size
    
    * update
    
    * update
    
    * yrdy
    
    * update
    
    * got threads working
    
    * remove ffmpeg path
    
    * test
    
    * test
    
    * test
    
    * update
    
    * test
    
    * updates
    
    * remove the dots
    
    * dont re-run scan if running
    
    * remove min
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- include/functions.psm1
+++ include/functions.psm1
@@ -1,9 +1,18 @@
 function Trace-Message ([string] $message) {
     Write-Output "$(Get-Date -Format G): $message"
-    $mtx = New-Object System.Threading.Mutex($false, "LogMutex")
+    $mtx = New-Object System.Threading.Mutex($false, "TranscodeMutex")
     If ($mtx.WaitOne(1000)) {
         Write-Output "$(Get-Date -Format G): $message" >> .\hevc_transcode.log
         [void]$mtx.ReleaseMutex()
+    }
+}
+
+function Trace-Savings ([string] $message) {
+    Write-Output "$(Get-Date -Format G): $message"
+    $mtx2 = New-Object System.Threading.Mutex($false, "SavingsMutex")
+    If ($mtx2.WaitOne(1000)) {
+        Write-Output "$(Get-Date -Format G): $message" >> .\hevc_savings.log
+        [void]$mtx2.ReleaseMutex()
     }
 }
 
