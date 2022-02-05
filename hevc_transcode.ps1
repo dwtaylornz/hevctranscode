803d9f0be6609bcd25bae179882088c5dde21293
Darren <dwtaylornz@gmail.com>
2022-02-05 17:28:27 +1300

    .

--- hevc_transcode.ps1
+++ hevc_transcode.ps1
@@ -35,7 +35,7 @@ $skiptotal_files = $skipped_files + $skippederror_files + $skippedhevc_files
 # Show total videos to process (scanned files - skip count) 
 # Get-VideosToProcess($file_count, $skip_count)
 Write-Host ""
-$video_count = ($file_count - $skiptotal_count)
+$video_count = ($file_count - $skip_count)
 Write-Host "Total videos to process: $video_count"
 
 #Show settings and any jobs running 
