ddae69ea436b853d76b95033f3dde93dfab1bfed
Darren <dwtaylornz@gmail.com>
2022-11-21 18:59:49 +1300

    again!

--- variables.ps1
+++ variables.ps1
@@ -12,7 +12,7 @@ $restart_queue = 120 # mins before re-doing the scan and start going through the
 $ffmpeg_codec = "hevc_amf" # set to hevc_amf for AMD, hevc_nvenc for Nvidia, libx265 for CPU
 $ffmpeg_hwdec = 0 # set to 1 if you want to decode via HW also 
 $ffmpeg_logging = "quiet" # "quiet", "panic", "fatal", "error", "warning", "info", "verbose", "debug", "trace"
-$ffmpeg_timeout = 30 # timeout on job (minutes)
+$ffmpeg_timeout = 120 # timeout on job (minutes)
 $ffmpeg_min_diff = 20 # must be at least this much smalller (percentage)
 $ffmpeg_max_diff = 95 # must be at most this much bigger (percentage)
 $ffmpeg_aac = 2 # 0 copies audio (no changes), set to 1 if you want to transcode audio to AAC (2 channel), 2 if you want to use libfdk_aac (2 channel, must be included in ffmpeg)
@@ -21,7 +21,7 @@ $ffmpeg_eng = 0 # set to 1 to only keep english audio tracks
 $move_file = 1 # set to 0 for testing (check .\output directory) 
 $convert_1080p = 1 # 1 will convert higher resolution videos down to 1080p , 0 will not
 $min_video_size = 0 # min size in GB of video before it will quit
-$GPU_threads = 2 # how many GPU jobs at same time 
+$GPU_threads = 3 # how many GPU jobs at same time 
 
 # Write to influx
 $influx_address = "http://192.168.9.10:9086"
