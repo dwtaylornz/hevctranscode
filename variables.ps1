e71896f42836cd304624f1ae44916ef0f89488a0
Darren <dwtaylornz@gmail.com>
2022-12-18 19:07:31 +1300

    ..

--- variables.ps1
+++ variables.ps1
@@ -14,15 +14,16 @@ $ffmpeg_hwdec = 0 # set to 1 if you want to decode via HW also (not recommended)
 $ffmpeg_logging = "quiet" # "quiet", "panic", "fatal", "error", "warning", "info", "verbose", "debug", "trace"
 $ffmpeg_timeout = 45 # timeout on job (minutes)
 $ffmpeg_min_diff = 30 # must be at least this much smalller (percentage)
+$ffmpeg_max_diff = 95 # must not save more than this, assuming something has gone wrong (percentage)
 $ffmpeg_aac = 2 # 0 copies audio (no changes), set to 1 if you want to transcode audio to AAC (2 channel), 2 if you want to use libfdk_aac (2 channel, must be included in ffmpeg)
 $ffmpeg_mp4 = 0 # set to 1 to convert to mp4 
 $ffmpeg_eng = 0 # set to 1 to only keep english audio tracks
-$move_file = 0 # set to 0 for testing (check .\output directory) 
+$move_file = 1 # set to 0 for testing (check .\output directory) 
 $convert_1080p = 1 # 1 will convert higher resolution videos down to 1080p , 0 will not
 $min_video_size = 0.5 # min size in GB of video before it will quit
-$GPU_threads = 1 # how many GPU jobs at same time 
+$GPU_threads = 2 # how many GPU jobs at same time 
 # $ffmpeg_crf = 22 # (28 default)
-# $ffmpeg_max_diff = 95 # must be at most this much bigger (percentage)
+
 
 # used to fix color headers for roku playback (must have mkvpropedit.exe)
 $mkv_color_fix = 0
